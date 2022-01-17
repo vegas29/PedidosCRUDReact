c8a58743ae3e5714c5fd796ac75937a80d1f3109
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2022-01-17 18:05:55 -0500

    Con local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import ListadoPedidos from "./components/ListadoPedidos";
@@ -7,6 +7,20 @@ function App() {
 
   const [pedidos, setPedidos] = useState([]);
   const [pedido, setPedido] = useState({});
+
+  useEffect(()=>{
+    const obtenerLS = () =>{
+      const pedidosLS = JSON.parse(localStorage.getItem('pedidos')) ?? [];
+
+      setPedidos(pedidosLS);
+    }
+
+    obtenerLS();
+  }, []);
+
+  useEffect(()=>{
+    localStorage.setItem('pedidos', JSON.stringify(pedidos));
+  }, [pedidos]);
 
   const eliminarPedido = id =>{
     const pedidosActualizados = pedidos.filter(pedido => pedido.id !== id);
